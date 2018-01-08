313373a2758eaa802a6f655336296b54e2f6015b
Jared Gerschler <jjgerschler@gmail.com>
2018-01-08 11:04:25 -0600

    need to add sensor code to beanbag project

--- Bean Bag Game/BeanBagRev1/BeanBagRev1.ino
+++ Bean Bag Game/BeanBagRev1/BeanBagRev1.ino
@@ -1,36 +1,90 @@
 #include "MeOrion.h"
 
 Me7SegmentDisplay disp(PORT_6);
-MeRGBLed led(PORT_3, SLOT1);
+MeRGBLed led(PORT_3);// can't combine with switch!
+MeLimitSwitch switch1(PORT_4,SLOT1);
+MeLightSensor lightSensor(PORT_8);
+
 
 unsigned long previousMillis = 0;
 unsigned long currentMillis;
 int interval = 1000;
-int i = 30;
+int i;
+int score;
+int lightValue;
 uint8_t r;
 uint8_t g; 
 uint8_t b;
 
 void setup()
 {
+  i = 30;
+  score = 0;
+  disp.display(i);
+  for(int16_t x = 0; x < 15; x++)
+  {
+    led.setColorAt(x, 0, 100, 0);
+    led.show();
+    delay(1);
+  }
 }
 
 void loop()
 {
-  if(i < 0)
+  if (switch1.touched())
   {
+    previousMillis = millis();
+    while (i >= 0)
+    {
+      currentMillis = millis();
+      disp.display(score);
+      indicators(i/2);
+      lightValue = lightSensor.read();
+      if (lightValue > 50)
+      {
+        score++;
+      }
+      if (currentMillis - previousMillis >= interval) 
+      {
+        previousMillis = currentMillis;
+        i--;
+      }
+    }
     buzzerOn();
-    delay(1000);
+    for(int16_t x = 0; x < 3; x++)
+    {
+      for(int16_t x = 0; x < 15; x++)
+      {
+        led.setColorAt(x, 100, 0, 0);
+        led.show();
+        delay(1);
+      }
+      delay(150);
+      for(int16_t x = 0; x < 15; x++)
+      {
+        led.setColorAt(x, 0, 0, 0);
+        led.show();
+        delay(1);
+      }
+      delay(150);
+    } 
     buzzerOff();
-    i = 30;
+    reset();
   }
-  currentMillis = millis();
+}
+
+void reset()
+{
+  i = 30;
+  previousMillis = 0;
   disp.display(i);
-  indicators(i/2);
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    i--;
+  for(int16_t x = 0; x < 15; x++)
+  {
+    led.setColorAt(x, 0, 100, 0);
+    led.show();
+    delay(1);
   }
+  return;
 }
 
 void indicators(uint8_t count)
